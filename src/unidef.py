8b06ee7ae72700e892f3853065afd7041df21226
qiujiangkun <qiujiangkun@foxmail.com>
2021-08-31 19:24:51 +0800

    read from stdin

--- src/unidef.py
+++ src/unidef.py
@@ -4,18 +4,24 @@ import sys
 
 logging.basicConfig(stream=sys.stderr)
 from emitters.emitter_registry import EMITTER_REGISTRY
-from models.config_model import read_model_definition
+from models.config_model import read_model_definition, ModelDefinition, ModelExample
 from utils.typing_compat import *
 import argparse
 
 parser = argparse.ArgumentParser(description='define once, export everywhere')
-parser.add_argument('--target', '-t', type=str, nargs='?', help='target format')
-parser.add_argument('file', type=str, help='input file')
+parser.add_argument('--target', '-t', default='no_target', type=str, nargs='?', help='target format')
+parser.add_argument('--format', '-f', type=str, nargs='?', help='input format')
+parser.add_argument('file', default='/dev/stdin', type=str, nargs='?', help='input file')
 
 
-def main(target: Optional[str], content: str):
-    target = target or 'no_target'
-    models = read_model_definition(content)
+def main(target: str, fmt: Optional[str], content: str):
+    if fmt:
+        model = ModelDefinition(name='stdin', example=ModelExample(format=fmt, text=content))
+        models = [
+            model
+        ]
+    else:
+        models = read_model_definition(content)
     emitter = EMITTER_REGISTRY.find_emitter(target)
     if emitter is None:
         raise Exception(f'Could not find emitter for {target}')
@@ -25,4 +31,4 @@ def main(target: Optional[str], content: str):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.target, open(args.file).read())
+    main(args.target, args.format, open(args.file).read())
