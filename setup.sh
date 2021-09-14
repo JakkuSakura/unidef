c635ea04ee01258d96b7412ab56b7ba0b07a99f1
qiujiangkun <qiujiangkun@foxmail.com>
2021-09-15 01:03:16 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pip_site=$(env python3 -m pip --version | awk '{print $4}')
 site_packages="$(dirname "$pip_site")"
-BASEDIR=$(dirname "$0")
-unlink "$site_packages/unidef"
+BASEDIR=$(realpath $(dirname "$0"))
+unlink "$site_packages/unidef" 2> /dev/null
+echo Linking from "$BASEDIR/unidef" to "$site_packages/unidef"
 ln -s "$BASEDIR/unidef" "$site_packages/unidef"
