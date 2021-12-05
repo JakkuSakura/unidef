f3d026b4d2f1fb69aef621d96854a0f5cc3d9bfb
qiujiangkun <qiujiangkun@foxmail.com>
2021-12-06 01:29:06 +0800

    more tests

--- tests/test_parse_api.py
+++ tests/test_parse_api.py
@@ -3,13 +3,23 @@ import sys
 
 import os
 
-
 sys.path.insert(0, '.')
 from unidef.__main__ import *
 
 
+def try_parse(fm, to):
+    args = parser.parse_args([f'examples/{fm}.yaml', '-t', to])
+    config = CommandLineConfig.from_args(args)
+    main(config, open(args.file).read())
+
+
 def test_parse_model():
     for target in ['rust']:
-        args = parser.parse_args(['examples/model_example.yaml', '-t', target])
-        config = CommandLineConfig.from_args(args)
-        main(config, open(args.file).read())
+        try_parse("model_example", target)
+
+
+def test_parse_json_example():
+    for target in ['rust']:
+        try_parse("json_default_example", target)
+        try_parse("api_example", target)
+        try_parse("comments_example", target)
