c6d958dff041d12035d08aeb48bdfbac03743e3d
qiujiangkun <qjk2001@gmail.com>
2022-03-21 16:27:19 +0800

    better error reporting for codegen

--- src/main/scala/unidef/utils/Exceptions.scala
+++ src/main/scala/unidef/utils/Exceptions.scala
@@ -6,7 +6,7 @@ class ExceptionBase(msg: String, cause: Option[Throwable] = None)
     extends Exception(msg, cause.orNull)
 
 case class ParseCodeException(msg: String, code: String = "", cause: Option[Throwable] = None)
-    extends ExceptionBase(msg)
+    extends ExceptionBase(s"$msg (code: $code)", cause)
 case class TypeEncodeException(msg: String, ty: TyNode, cause: Option[Throwable] = None)
     extends ExceptionBase(s"$msg (type: $ty)", cause)
 case class TypeDecodeException[I](
